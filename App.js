31cb35eb013c45b92cab6831ab8b27ab7ab6743d
mohamedsl22 <moodi.salman@gmail.com>
2020-07-29 19:39:56 +0300

    fbAuth

--- App.js
+++ App.js
@@ -3,12 +3,13 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Profile from './components/Profile'
 import SignUp from './components/SignUp'
+import FacebookAuth from './components/FacebookAuth'
 
 export default function App() {
   return (
     <View style={styles.container}>
 
-      <SignUp />
+      <FacebookAuth />
       <StatusBar style="auto" />
 
     </View>
